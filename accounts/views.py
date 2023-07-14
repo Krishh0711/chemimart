ad2c394f2ad4ed55adf13876fb47f803e84bcd74
Krishnanshu <krishnanshu@squadstack.com>
2023-07-14 23:01:03 +0530

    create seller login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from accounts.serializers import SellerAccountLoginSerializer
 
-# Create your views here.
+
+class SellerAccountLoginView(APIView):
+    def post(self, request):
+        serializer = SellerAccountLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            status_code = status.HTTP_200_OK
+            seller, is_created = serializer.save()
+            if is_created:
+                status_code = status.HTTP_201_CREATED
+            refresh = RefreshToken.for_user(seller.user)
+            return Response({'access': str(refresh.access_token), 'refresh': str(refresh)}, status=status_code, content_type="application/json")
+        return Response({'error' : serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
