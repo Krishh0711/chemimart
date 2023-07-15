3c5a22e3a32aa9e141998df25805c70738b8ff16
Krishnanshu <krishnanshu@squadstack.com>
2023-07-16 01:59:00 +0530

    add doctring for common app

--- common/models.py
+++ common/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class AbstractTimeStampModel(models.Model):
+    """
+    Abstract model class with timestamp fields for creation and update times.
+    """
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- common/utils.py
+++ common/utils.py
@@ -1,4 +1,12 @@
 def validate_phone_number_and_otp(phone_number, otp):
+    """
+    Validate the phone number and OTP (One-Time Password) inputs.
+    Args:
+        phone_number (str): The phone number to validate.
+        otp (int): The OTP to validate.
+    Returns:
+        bool: True if the phone number and OTP are valid, False otherwise.
+    """
     if phone_number is None or len(phone_number) != 10:
         return False
     if otp is None or len(str(otp)) != 6:
