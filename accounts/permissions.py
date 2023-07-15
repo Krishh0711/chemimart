1dc334a2745ef8125404e5a0b83e7cc13636f9d2
Krishnanshu <krishnanshu@squadstack.com>
2023-07-16 01:57:01 +0530

    add doc string in account app

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -2,5 +2,8 @@ from rest_framework.permissions import BasePermission
 from accounts.models import SellerAccount
 
 class IsSeller(BasePermission):
+    """
+    Custom permission class that checks if the user is a seller.
+    """
     def has_permission(self, request, view):
         return request.user.is_seller
