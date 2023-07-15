f0bee01da36a4b1d9095856ed1b752fe55f00254
Krishnanshu <krishnanshu@squadstack.com>
2023-07-15 14:12:36 +0530

    change field name for available product

--- stores/models.py
+++ stores/models.py
@@ -26,7 +26,7 @@ class Product(AbstractTimeStampModel):
     mrp = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     sale_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     image = models.ImageField(upload_to='product_images/')
-    is_active = models.BooleanField(default=True)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
